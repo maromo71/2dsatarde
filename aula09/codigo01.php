911530b70cc882b8e6e804b3ad2635e412f205ab
Maromo <professormoraes@gmail.com>
2023-05-29 16:06:34 -0300

    Aula do dia 29 de maio

--- aula09/codigo01.php
+++ aula09/codigo01.php
@@ -5,8 +5,12 @@
     $estoque->adicionar("Bola", 100);
     $estoque->adicionar("Meia", 50);
     $estoque->adicionar("Camisa", 30);
+    $estoque->adicionar("Camiseta", 10);
+    $estoque->adicionar("Bola", 60);
 
     echo "Numero de cada item que tenho \n";
-    echo $estoque->itens["Bola"] . " bolas \n";
-    echo $estoque->itens["Meia"] . " meias \n";
-    echo $estoque->itens["Camisa"] . " camisas \n";
+    foreach($estoque->itens as $key => $value){
+        echo "O produto $key possui em estoque $value peças \n";
+    }
+
+    
