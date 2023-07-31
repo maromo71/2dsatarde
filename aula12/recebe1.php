9c854359dcd88ef80dde7aabbe668b3a4df539ee
Maromo <professormoraes@gmail.com>
2023-07-31 16:18:04 -0300

    Aula do dia 31 de julho

--- aula12/recebe1.php
+++ aula12/recebe1.php
@@ -1,9 +1,15 @@
 <?php
-  if ($_POST["txtNome"] == null)
-    echo "Nome Vazio";
-    
-  echo $_POST["txtNome"];
-  die();
-  echo "<p></p>";
-  echo $_POST["numIdade"];
+  if ($_POST["txtNome"] == null || $_POST["numIdade"] == null){
+    //campos não preenchidos
+    echo "Existem campos não preenchidos<br>";
+    echo "<a href='form1.php'>Voltar</a>";
+    return;
+  }
+
+  //variáveis para receber, agora que foram tratados
+  $nome = $_POST["txtNome"];
+  $idade = $_POST["numIdade"];
+  
+  //Mostrando uma saída formatada com as duas variáveis
+  echo "<h2>A pessoa $nome possui $idade anos de idade</h2>";
   
